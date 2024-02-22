372542c92608ea04577d6bfed8b2ee3ce4b459f7
SamuelOlavo <samuelobt@gmail.com>
2024-02-22 16:27:53 -0300

    feat: implementando melhorias no formulario

--- script.js
+++ script.js
@@ -1,97 +1,157 @@
-onload = () => {   
-    
-    document.getElementById('btnEnviar').disabled = true;
 
-    nome.onblur = () => {
-        if(nome.value == '') {
-            nome.style.backgroundColor = '#F88';   
-        }
-        else {
-            nome.style.backgroundColor = '#FFF';  
-                                
-        } if (nome.value != '' && tel.value != '' && dateNas.value != '' && date.value != '' && hora.value != '' && date.value > today && serv.value != '') btnEnviar.disabled = false;
-        else btnEnviar.disabled = true;
-    };  
-    dateNas.onblur = () => {
-        if(dateNas.value == '') {
-            dateNas.style.backgroundColor = '#F88';                   
-        }
-        else {
-            dateNas.style.backgroundColor = '#FFF';
-        } if (nome.value != '' && tel.value != '' && dateNas.value != '' && date.value != '' && hora.value != '' && date.value > today && serv.value != '') btnEnviar.disabled = false;
-        else btnEnviar.disabled = true;
-    };  
-    tel.onblur = () => {
-        if(tel.value == '') {
-            tel.style.backgroundColor = '#F88';                   
-        }
-        else {
-            tel.style.backgroundColor = '#FFF';
-        } if (nome.value != '' && tel.value != '' && dateNas.value != '' && date.value != '' && hora.value != '' && date.value > today && serv.value != '') btnEnviar.disabled = false;
-        else btnEnviar.disabled = true;
-    };   
 
-    serv.onblur = () => {
-        if (serv.value == '') {
-            serv.style.backgroundColor = '#F88';
-         }
-        else {
-            serv.style.backgroundColor = '#FFF';
-        } if (nome.value != '' && tel.value != '' && dateNas.value != '' && date.value != '' && hora.value != '' && date.value > today && serv.value != '') btnEnviar.disabled = false;
-        else btnEnviar.disabled = true;
-    };  
+const URL_TESTE = "https://api-siand-esqe.onrender.com";
 
-    // Data e Hora do Agendamento
-    var today = new Date().toISOString().split('T')[0];                    
-    console.log (today);
-    date.onblur = () => {
-        if(date.value == '' || date.value < today) {
-            date.style.backgroundColor = '#F88';
-            btnEnviar.disabled = true;                                       
-        }
-        else {
-            date.style.backgroundColor = '#FFF';            
-        } if (nome.value != '' && tel.value != '' && dateNas.value != '' && date.value != '' && hora.value != '' && date.value > today && serv.value != '') btnEnviar.disabled = false;
-        else btnEnviar.disabled = true;
-    };  
-    
-    hora.onblur = () => {
-        if( hora.value < '09:00' || hora.value > '18:00')  {
-            hora.style.backgroundColor = '#F88';
+
+// Função para executar as ações ao carregar a página
+window.onload = () => {
+    exibirToast("Faça seu agendamento", "green");
+    carregarServicos();
+    configurarEventListeners();
+};
+
+// Função para exibir o toast
+function exibirToast(mensagem, cor) {
+    Toastify({
+        text: mensagem,
+        duration: 3000,
+        close: true,
+        style: { background: cor }
+    }).showToast();
+}
+
+// Função para carregar os serviços
+async function carregarServicos() {
+    try {
+        const response = await fetch(`${URL_TESTE}/servicos/servicos`);
+        if (response.status === 200) {
+            const data = await response.json();
+            preencherDropdownServicos(data);
         } else {
-            hora.style.backgroundColor = '#FFF';
-        } if (nome.value != '' && tel.value != '' && dateNas.value != '' && date.value != '' && hora.value != '' && date.value > today && serv.value != '') btnEnviar.disabled = false;
-        else btnEnviar.disabled = true;
-    };  
+            throw new Error('Erro de busca');
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
 
 
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
+// Função para preencher o dropdown de serviços
+function preencherDropdownServicos(data) {
+    const list_serv = [...new Set(data.map(serv => serv.Servicos))];
+    const select = document.getElementById("serv");
+    select.innerHTML = '';
+    const option1 = document.createElement('option');
+    const inicio = "⇕";
+    option1.text = inicio;
+    select.appendChild(option1); // Adiciona a opção de seta ao select
+
+    list_serv.forEach(servicos => {
+        const option = document.createElement('option');
+        option.text = servicos;
+        select.appendChild(option); // Adiciona cada opção de serviço ao select
+    });
+
+    select.value = inicio; // Define a opção de seta como a opção selecionada por padrão
+}
+
+
+// Função para carregar os esteticistas
+async function carregarEsteticistas(servicoSelecionado) {
+    try {
+        const response = await fetch(`${URL_TESTE}/servicos/servico/${servicoSelecionado}`);
+        if (response.status === 200) {
+            const data = await response.json();
+            return [...new Set(data.map(prof => prof.Esteticista))];
+        } else {
+            throw new Error('Erro de busca');
+        }
+    } catch (error) {
+        console.error('Erro:', error);
+    }
+}
+
+// Função para preencher o dropdown de esteticistas
+function preencherDropdownEsteticistas(esteticistas) {
+    const select = document.getElementById("prof");    
+    select.innerHTML = '';
+    esteticistas.forEach(esteticista => {
+        const option = document.createElement('option');
+        option.text = esteticista;
+        select.appendChild(option);
+    });
+}
+
+// Event listener para carregar os esteticistas quando o serviço é selecionado
+document.getElementById('serv').addEventListener('change', async () => {
+    const servicoSelecionado = document.getElementById('serv').value;
+    const esteticistas = await carregarEsteticistas(servicoSelecionado);
+    if (esteticistas) {
+        preencherDropdownEsteticistas(esteticistas);
+    }
+});
+
+// Função para configurar os event listeners
+function configurarEventListeners() {
+    document.getElementById('tel').addEventListener('blur', validarCampo);
+    document.getElementById('date').addEventListener('blur', validarCampo);
+    document.getElementById('dateNas').addEventListener('blur', validarCampo);
+    document.getElementById('hora').addEventListener('blur', validarCampo);
+    document.getElementById('prof').addEventListener('blur', validarCampo);
+}
+
+// Função para validar campos
+function validarCampo(event) {
+    const campo = event.target;
+    if (campo.value === '') {
+        campo.style.backgroundColor = '#F88';
+    } else {
+        campo.style.backgroundColor = '#FFF';
+    }
+}
+
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();  
+        const data = document.getElementById('date').value;
+        let partes = data.split('-');
+        let dataFormatada = partes[2] + '/' + partes[1] + '/' + partes[0];
+
+        const dataNas = document.getElementById('dateNas').value;
+        let traco = dataNas.split('-');
+        let dataNasFormatada = traco[2] + '/' + traco[1] + '/' + traco[0];
+                      
 
         const NomeCliente = document.querySelector('input[name=nome]').value;
-        const Telefone = document.querySelector('input[name=tel]').value;
-        const Data = document.getElementById('date').value;
-        const DataNascimento = document.getElementById('dateNas').value;  
-        const Hora = document.getElementById('hora').value;
-        const Servico = document.getElementById('serv').value;
-        //Observado que a declaração da variavel deve estar dentro da função e sobre a consulta no DOM pode usar o ID normalmente
+        const DataNascimento = dataNasFormatada;
+        const Telefone = document.querySelector('input[name=tel]').value;        
+        const Esteticista = document.getElementById('prof').value;  
+        const Servicos = document.getElementById('serv').value;
+        const Data = dataFormatada;
+        const Horario = document.getElementById('hora').value;
 
-        fetch('https://api.sheetmonkey.io/form/vb44GDNaPbaYwZHvEbHMHU', {
-            method: 'post',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ NomeCliente , DataNascimento, Telefone , Data ,  Hora, Servico}),
-        });               
-        
-        document.querySelector('.loader').style.display = 'block';
-        let timeout = window.setTimeout(function() {            
-            document.querySelector('.loader').style.display = 'none';
-            document.querySelector("form").reset();            
-          }, 5000);
-          document.getElementById('btnEnviar').disabled = true;                
-    }
-    document.getElementById('agendamento').addEventListener('submit', handleSubmit);
-};
+        try {
+            const response = await fetch(`${URL_TESTE}/agenda/`, {
+                method: 'post',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ NomeCliente , DataNascimento, Telefone , Esteticista , Servicos , Data ,  Horario }),
+            }); 
+            if (response.status === 201) {
+                exibirToast('Agendamento realizado com sucesso.', '#269934');
+                console.log(response);
+                document.querySelector("form").reset();  
+            } else {
+                exibirToast('Favor preencher todos os campos do formulario.', '#ff0000');
+                console.log(response);
+            }
+        } catch (error) {
+            console.error('Error:', error);
+            exibirToast('Erro no cadastro. Por favor, tente novamente.', '#ff0000');
+        }        
+   
+    };
+document.getElementById('agendamento').addEventListener('submit', handleSubmit);    
